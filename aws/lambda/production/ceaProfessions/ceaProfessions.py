e934b346e2fd87244794280e2b32bd3a98fe77e9
Lawrence McDaniel <lpm0073@gmail.com>
2017-09-15 13:21:51 -0500

    update api for Industry and Profession parent_id

--- aws/lambda/production/ceaProfessions/ceaProfessions.py
+++ aws/lambda/production/ceaProfessions/ceaProfessions.py
@@ -28,11 +28,32 @@ password = rds_config.db_password
 db_name = rds_config.db_name
 
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.ERROR)
 
 def lambda_handler(event, context):
+    """
+    This function handles mySQL CRUD for CEA data element Professions.
+    Receives a CRUD instruction from the event body "lambda" parameter value.
+    Returns a Professions recordset in all cases.
 
+    Sample REST api event body
+    ===========================
+    {
+      "body": {},
+      "params": {
+        "path": {
+          "parentId": "1"
+        },
+        "querystring": {},
+        "header": {}
+      },
+      "lambda": "select"
+    }
+
+    lambda parameter values: select
+    """
     retval = {}
+    logger.info('JSON received: ' + str(event))
 
     #
     # 1. connect to the MySQL database
@@ -40,12 +61,11 @@ def lambda_handler(event, context):
     try:
         conn = pymysql.connect(rds_host, user=name,
                                passwd=password, db=db_name, connect_timeout=2)
+
     except Exception as e:
         logger.error("ERROR: Could not connect to MySql instance.")
         logger.error(e)
-        retval["response"] = "failure"
-        retval["err"] = str(e)
-        return retval
+        return e
 
     logger.info("Connected to RDS mysql instance.")
 
@@ -54,11 +74,25 @@ def lambda_handler(event, context):
     # 2. parse the input parameters from the https request body
     #    which is passed to this Lambda function from a AWS API Gateway method object
     #
+    command = event['lambda']
+    try:
+        id = event['params']['path']['parentId']
+    except Exception as e:
+        logger.info("No path ID included")
+        id = -1
 
     #
     # 3. create the SQL string
     #
-    sql = "CALL sp_professions_get()"
+    if id < 0:
+        #Generic api calls. not specific to an element id.
+        if command == "select":
+            sql = "CALL cea.sp_professions_get(null)"
+
+    else:
+        if command == "select":
+            sql = "CALL cea.sp_professions_get('%s')" % (id)
+
 
     #
     # 4. execute the SQL string
@@ -70,26 +104,29 @@ def lambda_handler(event, context):
     except Exception as e:
         logger.error("ERROR: MySQL returned an error.")
         logger.error(e)
-        retval["response"] = "failure"
-        retval["err"] = str(e)
-        return retval
+        return e
 
-    #
-    # 5a. format the recordset returned as a JSON string
-    #
+#
+# 5a. format the recordset returned as a JSON string
+#
 
+    #note: there will only be one record in this recorset.
     rs = cursor.fetchall()
 
-    dictionary = {}
+    retval = []
     for record in rs:
-        dictionary[record[1]] = record[0]
+        obj = {
+            "id" : record[0],
+            "description" : str(record[1])
+        }
+        retval.append(obj)
 
     cursor.close ()
     conn.close ()
 
-    #
-    # 5b. return the JSON string to the AWS API Gateway method that called this lambda function.
-    #     the API Gateway method will push this JSON string in the http response body
-    #
-    logger.info('JSON returned is: ' + json.dumps(dictionary))
-    return dictionary
+#
+# 5b. return the JSON string to the AWS API Gateway method that called this lambda function.
+#     the API Gateway method will push this JSON string in the http response body
+#
+    logger.info('JSON returned is: ' + json.dumps(retval))
+    return retval
